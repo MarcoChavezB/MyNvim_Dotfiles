3e28c4a02cdcedc4061644806919e5a75600a58a
MarcoChavezB <marco1102004@gmail.com>
2024-06-08 20:01:39 -0600

    formateando archivo keymaps

--- lua/habib/core/keymaps.lua
+++ lua/habib/core/keymaps.lua
@@ -1,82 +1,83 @@
+-- Set leader key to space
 vim.g.mapleader = " "
+
+-- Alias for setting keymaps
 local map = vim.api.nvim_set_keymap
--- Toggle between tabs
-map("n", "<C-h>", "<C-w>h", { noremap = true, silent = false })
-map("n", "<C-j>", "<C-w>j", { noremap = true, silent = false })
-map("n", "<C-k>", "<C-w>k", { noremap = true, silent = false })
-map("n", "<C-l>", "<C-w>l", { noremap = true, silent = false })
 
--- BufferLine
-map("n", "<Tab>", ":BufferLineCycleNext<cr>", { noremap = true, silent = true })
-map("n", "<S-Tab>", ":BufferLineCyclePrev<cr>", { noremap = true, silent = true })
+-- Keybindings
 
--- Indenting
-map("v", "<", "<gv", { noremap = true, silent = false })
-map("v", ">", ">gv", { noremap = true, silent = false })
+-- Window navigation
+map("n", "<C-h>", "<C-w>h", { noremap = true, silent = false }) -- Move to the left window
+map("n", "<C-j>", "<C-w>j", { noremap = true, silent = false }) -- Move to the lower window
+map("n", "<C-k>", "<C-w>k", { noremap = true, silent = false }) -- Move to the upper window
+map("n", "<C-l>", "<C-w>l", { noremap = true, silent = false }) -- Move to the right window
 
--- Copy-Pasting
-map("v", "<C-c>", '"+y', { noremap = true, silent = false })
-map("n", "<C-s>", '"+P', { noremap = true, silent = false })
+-- BufferLine navigation
+map("n", "<Tab>", ":BufferLineCycleNext<cr>", { noremap = true, silent = true }) -- Next buffer
+map("n", "<S-Tab>", ":BufferLineCyclePrev<cr>", { noremap = true, silent = true }) -- Previous buffer
+
+-- Indenting in visual mode
+map("v", "<", "<gv", { noremap = true, silent = false }) -- Decrease indent
+map("v", ">", ">gv", { noremap = true, silent = false }) -- Increase indent
+
+-- Copy-pasting
+map("v", "<C-c>", '"+y', { noremap = true, silent = false }) -- Copy to system clipboard
+map("n", "<C-s>", '"+P', { noremap = true, silent = false }) -- Paste from system clipboard
+
 -- Remapping Escape key
-map("i", "kj", "<Esc>", { noremap = true, silent = false })
-map("n", "kj", "<Esc>", { noremap = true, silent = false })
-map("v", "kj", "<Esc>", { noremap = true, silent = false })
+map("i", "kj", "<Esc>", { noremap = true, silent = false }) -- Exit insert mode
+map("n", "kj", "<Esc>", { noremap = true, silent = false }) -- Exit normal mode
+map("v", "kj", "<Esc>", { noremap = true, silent = false }) -- Exit visual mode
 
--- Remapping gj gk for wrapped line
-map("n", "j", "gj", { noremap = true, silent = true })
-map("n", "k", "gk", { noremap = true, silent = true })
--- Unhighlight searched elements
-map("n", "<C-m>", ":nohlsearch<cr>", { noremap = true, silent = true })
--- show nvim-tree on ctrl+e
-map("n", "<C-e>", ":NvimTreeToggle<cr>", { noremap = true, silent = true })
--- hide nvim-tree on ctrl+d
-map("n", "<C-d>", ":NvimTreeClose<cr>", { noremap = true, silent = true })
+-- Wrapping lines
+map("n", "j", "gj", { noremap = true, silent = true }) -- Move down by screen lines
+map("n", "k", "gk", { noremap = true, silent = true }) -- Move up by screen lines
 
--- show find files on ctrl+tabs
+-- Unhighlight search
+map("n", "<C-m>", ":nohlsearch<cr>", { noremap = true, silent = true }) -- Clear search highlight
+
+-- NvimTree
+map("n", "<C-e>", ":NvimTreeToggle<cr>", { noremap = true, silent = true }) -- Toggle NvimTree
+map("n", "<C-d>", ":NvimTreeClose<cr>", { noremap = true, silent = true }) -- Close NvimTree
+
+-- Telescope
 map(
 	"n",
 	"<C-tab>",
 	'<cmd>lua require("telescope.builtin").find_files({ cwd = vim.fn.getcwd() })<CR>',
 	{ noremap = true, silent = true }
-)
+) -- Find files
+map("n", "<C-'>", ":Telescope live_grep<cr>", { noremap = true, silent = true }) -- Live grep
+map("n", "<C-¡>", ":Telescope current_buffer_fuzzy_find<cr>", { noremap = true, silent = true }) -- Fuzzy find in buffer
 
--- select text
-map("n", "<C>s", "v$", { noremap = true, silent = true })
--- divide window
-map("n", "<C-w>\\", ":vsplit<cr>", { noremap = true, silent = true })
--- divide window to bottom
-map("n", "<C-w>-", ":split<cr>", { noremap = true, silent = true })
+-- Text selection
+map("n", "<C>s", "v$", { noremap = true, silent = true }) -- Select to end of line
 
--- open terminal
-map("n", "<C-t>", ":ToggleTerm<cr>", { noremap = true, silent = true })
+-- Window splitting
+map("n", "<C-w>\\", ":vsplit<cr>", { noremap = true, silent = true }) -- Vertical split
+map("n", "<C-w>-", ":split<cr>", { noremap = true, silent = true }) -- Horizontal split
 
--- seach word in all files
-map("n", "<C-'>", ":Telescope live_grep<cr>", { noremap = true, silent = true })
+-- Terminal
+map("n", "<C-t>", ":ToggleTerm<cr>", { noremap = true, silent = true }) -- Toggle terminal
 
--- open seach in file content
-map("n", "<C-¡>", ":Telescope current_buffer_fuzzy_find<cr>", { noremap = true, silent = true })
+-- File tree resizing
+map("n", "<C-9>", ":vertical resize +2<cr>", { noremap = true, silent = true }) -- Increase width
+map("n", "<C-8>", ":vertical resize -2<cr>", { noremap = true, silent = true }) -- Decrease width
 
--- File Tree --
+-- File tree focus
+map("n", "<C-l>", ":NvimTreeFocus<cr>", { noremap = true, silent = true }) -- Focus NvimTree
+map("n", "<C-ñ>", ":wincmd w<cr>", { noremap = true, silent = true }) -- Switch between windows
 
--- rezise file tree (row right, row left)
-map("n", "<C-9>", ":vertical resize +2<cr>", { noremap = true, silent = true })
-map("n", "<C-8>", ":vertical resize -2<cr>", { noremap = true, silent = true })
+-- Tabs
+map("n", "<C-c>", ":tabclose<cr>", { noremap = true, silent = true }) -- Close tab
 
--- cursor focus on file tree and the work space
-map("n", "<C-l>", ":NvimTreeFocus<cr>", { noremap = true, silent = true })
-map("n", "<C-ñ>", ":wincmd w<cr>", { noremap = true, silent = true })
-
--- tabs --
--- delete tabs
-map("n", "<C-c>", ":tabclose<cr>", { noremap = true, silent = true })
-
--- Mapear teclas para el uso de goto-preview
-
+-- Goto-preview
 map(
 	"n",
 	"<C-A-CR>",
 	'<cmd>lua require("goto-preview").goto_preview_definition()<CR>',
 	{ noremap = true, silent = true }
 )
-map("n", "<Esc>", '<cmd>lua require("goto-preview").close_all_win()<CR>', { noremap = true, silent = true })
-map("n", "C-S-CR", "<cmd>Telescope lsp_references<CR>", { noremap = true, silent = true })
+-- Preview definition
+map("n", "<Esc>", '<cmd>lua require("goto-preview").close_all_win()<CR>', { noremap = true, silent = true }) -- Close all previews
+map("n", "C-S-CR", "<cmd>Telescope lsp_references<CR>", { noremap = true, silent = true }) -- List references
