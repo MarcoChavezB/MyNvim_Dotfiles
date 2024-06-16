afd35a882c7d4c44f58c18c36805f05b86df6c68
MarcoChavezB <marco1102004@gmail.com>
2024-06-16 00:47:31 -0600

    formateando archivo keymaps

--- lua/habib/core/keymaps.lua
+++ lua/habib/core/keymaps.lua
@@ -42,16 +42,11 @@ map("n", "<C-e>", ":NvimTreeToggle<cr>", { noremap = true, silent = true }) -- T
 map("n", "<C-d>", ":NvimTreeClose<cr>", { noremap = true, silent = true }) -- Close NvimTree
 
 -- Telescope
-map(
-	"n",
-	"<C-tab>",
-	'<cmd>lua require("telescope.builtin").find_files({ cwd = vim.fn.getcwd() })<CR>',
-	{ noremap = true, silent = true }
-) -- Find files
+map("n", "<C-tab>", '<cmd>lua require("telescope.builtin").find_files({ cwd = vim.fn.getcwd() })<CR>', { noremap = true, silent = true }) -- Find files
 map("n", "<C-'>", ":Telescope live_grep<cr>", { noremap = true, silent = true }) -- Live grep
 map("n", "<C-¡>", ":Telescope current_buffer_fuzzy_find<cr>", { noremap = true, silent = true }) -- Fuzzy find in buffer
-map("n", "<C-p>", ":Telescope buffers<CR>", { noremap = true, silent = true })
-map("n", "<C-S-p>", ":Bonly<CR>", { noremap = true, silent = true })
+map("n", "<C-p>", ":Telescope buffers<CR>", { noremap = true, silent = true }) -- Open buffers list
+map("n", "<C-S-p>", ":Bonly<CR>", { noremap = true, silent = true }) -- Close all buffers except current
 
 -- Text selection
 map("n", "<C>s", "v$", { noremap = true, silent = true }) -- Select to end of line
@@ -75,37 +70,27 @@ map("n", "<C-ñ>", ":wincmd w<cr>", { noremap = true, silent = true }) -- Switch
 map("n", "<C-c>", ":tabclose<cr>", { noremap = true, silent = true }) -- Close tab
 
 -- Goto-preview
-map(
-	"n",
-	"<C-A-CR>",
-	'<cmd>lua require("goto-preview").goto_preview_definition()<CR>',
-	{ noremap = true, silent = true }
-)
--- Preview definition
+map("n", "<C-A-CR>", '<cmd>lua require("goto-preview").goto_preview_definition()<CR>', { noremap = true, silent = true }) -- Preview definition
 map("n", "<Esc>", '<cmd>lua require("goto-preview").close_all_win()<CR>', { noremap = true, silent = true }) -- Close all previews
 map("n", "C-S-CR", "<cmd>Telescope lsp_references<CR>", { noremap = true, silent = true }) -- List references
 
 -- Delete workspace
-map("n", "<C-x>", "dd", { noremap = true, silent = true })
-map("n", "<C-BS>", "db", { noremap = true, silent = true })
+map("n", "<C-x>", "dd", { noremap = true, silent = true }) -- Delete current line
+map("n", "<C-BS>", "db", { noremap = true, silent = true }) -- Delete word before cursor
 
+-- Text selection with Ctrl+Shift+Arrow keys
+map("n", "<C-S-Right>", "ve", { noremap = true, silent = true }) -- Select to end of word
+map("n", "<C-S-Left>", "vb", { noremap = true, silent = true }) -- Select to beginning of word
+map("n", "<C-S-Up>", "vk", { noremap = true, silent = true }) -- Select previous line
+map("n", "<C-S-Down>", "vj", { noremap = true, silent = true }) -- Select next line
 
--- text select
-map("n", "<C-S-Right>", "ve", { noremap = true, silent = true })
-map("n", "<C-S-Left>", "vb", { noremap = true, silent = true })
-map("n", "<C-S-Up>", "vk", { noremap = true, silent = true })
-map("n", "<C-S-Down>", "vj", { noremap = true, silent = true })
+-- Text deletion
+map("v", "<C-x>", '"+d', default_opts) -- Cut to system clipboard
 
-
--- text deleted
-map("v", "<C-x>", '"+d', default_opts)
-
--- text Copy
-map("v", "<C-c>", '"+y', default_opts)
-map("n", "<C-Del>", "dd", default_opts)
-map('n', '<C-a>', 'ggVG', default_opts)
-
-
+-- Text copying
+map("v", "<C-c>", '"+y', default_opts) -- Copy to system clipboard
+map("n", "<C-Del>", "dd", default_opts) -- Delete current line
+map('n', '<C-a>', 'ggVG', default_opts) -- Select all text
 
 -- Movimientos entre ventanas divididas
 map("n", "<C-w><Right>", "<C-w>l", { noremap = true, silent = true }) -- Mover a la ventana derecha
@@ -114,6 +99,7 @@ map("n", "<C-w><Up>", "<C-w>k", { noremap = true, silent = true }) -- Mover a la
 map("n", "<C-w><Left>", "<C-w>h", { noremap = true, silent = true }) -- Mover a la ventana izquierda
 
 -- Mover la selección hacia abajo
-map("v", "S", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+map("v", "S", ":m '>+1<CR>gv=gv", { noremap = true, silent = true }) -- Mover la selección hacia abajo
 -- Mover la selección hacia arriba
-map("v", "W", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+map("v", "W", ":m '<-2<CR>gv=gv", { noremap = true, silent = true }) -- Mover la selección hacia arriba
+
